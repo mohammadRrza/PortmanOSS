b6cf859da6276e720ef32049a59ed0edb998595f
Mohammadreza Taherabadi <mohammadreza.taherabadi@pishgaman.net>
2021-02-06 02:28:45 -0800

    Router

--- portman_web/contact/models.py
+++ portman_web/contact/models.py
@@ -15,11 +15,29 @@ class ContactType(models.Model):
         return self.title
 
 class Contact(models.Model):
-    contact_dslam = models.ForeignKey(DSLAM,db_index=True, on_delete=models.CASCADE)
+    #contact_dslam = models.ForeignKey(DSLAM,db_index=True, on_delete=models.CASCADE)
     contact_type = models.ForeignKey(ContactType,db_index=True, on_delete=models.CASCADE)
     contact_name = models.CharField(max_length=256)
     phone= models.CharField(max_length=256)
     mobile_phone = models.CharField(max_length=256)
     contact_email = models.CharField(max_length=256)
+    rastin_contact_id= models.CharField(max_length=256)
+
     def __str__(self):
         return self.contact_name
+
+
+class Order(models.Model):
+    rastin_order_id = models.IntegerField(db_index=True, blank=True, null=True)
+    order_dslam = models.ForeignKey(DSLAM,db_index=True, on_delete=models.CASCADE)
+    order_contact = models.ForeignKey(Contact,db_index=True, on_delete=models.CASCADE)
+    ranjePhoneNumber = models.CharField(max_length=256)
+    username = models.CharField(max_length=256)
+    slot_number = models.IntegerField(db_index=True, blank=True, null=True)
+    port_number = models.IntegerField(db_index=True, blank=True, null=True)
+    telco_row = models.IntegerField(db_index=True, blank=True, null=True)
+    port_Column = models.IntegerField(db_index=True, blank=True, null=True)
+    telco_connection = models.IntegerField(db_index=True, blank=True, null=True)
+    fqdn = models.CharField(max_length=256)
+    def __str__(self):
+        return self.rastin_order_id
