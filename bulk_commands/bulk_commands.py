e440018079c1cb18acfef452b2edcc12af8e5893
0422094080 <m.taher@pishgaman.net>
2021-08-12 10:05:39 +0000

    backups

--- bulk_commands/bulk_commands.py
+++ bulk_commands/bulk_commands.py
@@ -1,4 +1,4 @@
-	from Commands.get_backups import GetbackUp
+from Commands.get_backups import GetbackUp
 from Commands.get_mikrotik_routers_backup import GetMikrotikbackUp
 from Commands.get_cisco_switches_backup import GetCiscoSwitchbackUp
 
@@ -6,8 +6,8 @@ if __name__ == '__main__':
     '''back_up = GetbackUp()
     print('Backup process is started.')
     back_up.run_command()'''
-    cisco_switches_backup = GetCiscoSwitchbackUp()
-    GetCiscoSwitchbackUp.run_command(self=None)
+    #cisco_switches_backup = GetCiscoSwitchbackUp()
+    #GetCiscoSwitchbackUp.run_command(self=None)
     mikrotik_routers_backup = GetMikrotikbackUp()
     mikrotik_routers_backup.run_command()
 
