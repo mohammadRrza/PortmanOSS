d3d6e120253c252b1cc55e05f431fa0e0a2c9866
1741298148 <s.saedi@pishgaman.net>
2021-09-04 11:51:41 +0430

    all error handling are over

---
+++ portman_core2/vendors/fiberhomeAN3300_commands/show_port_with_mac.py
@@ -0,0 +1,96 @@
+import telnetlib
+import time
+from socket import error as socket_error
+from .command_base import BaseCommand
+import re
+
+
+class ShowPortWithMac(BaseCommand):
+    def __init__(self, params=None):
+        self.__HOST = None
+        self.__telnet_username = None
+        self.__telnet_password = None
+        self.port_conditions = params.get('port_conditions')
+        self.__access_name = params.get('access_name', 'an3300')
+        self.__mac = params.get('mac')
+
+    @property
+    def HOST(self):
+        return self.__HOST
+
+    @HOST.setter
+    def HOST(self, value):
+        self.__HOST = value
+
+    @property
+    def telnet_username(self):
+        return self.__telnet_username
+
+    @telnet_username.setter
+    def telnet_username(self, value):
+        self.__telnet_username = value
+
+    @property
+    def telnet_password(self):
+        return self.__telnet_password
+
+    @telnet_password.setter
+    def telnet_password(self, value):
+        self.__telnet_password = value
+
+    retry = 1
+
+    def run_command(self):
+        try:
+            tn = telnetlib.Telnet(self.__HOST)
+            tn.write((self.__telnet_username + "\r\n").encode('utf-8'))
+            tn.write((self.__telnet_password + "\r\n").encode('utf-8'))
+            tn.write(b"end\r\n")
+            err1 = tn.read_until(b"end")
+            if "Login Failed." in str(err1):
+                return "Telnet Username or Password is wrong! Please contact with core-access department."
+            tn.read_until(b"User>")
+            tn.write(b'admin\r\n')
+            tn.read_until(b"Password:")
+            tn.write('{0}\r\n'.format(self.__access_name).encode('utf-8'))
+            time.sleep(0.5)
+            err1 = tn.read_until(b"#", 1)
+            if "Bad Password..." in str(err1):
+                return "DSLAM Password is wrong!"
+            tn.write(b"cd fdb\r\n")
+            tn.write("sh fdb slot {0}\r\n".format(self.port_conditions['slot_number']).encode('utf-8'))
+            time.sleep(3)
+            tn.write(b"\r\n")
+            time.sleep(0.1)
+            tn.write(b"\r\n")
+            time.sleep(0.1)
+            tn.write(b"\r\n")
+            time.sleep(0.1)
+            tn.write(b"\r\n")
+            time.sleep(0.1)
+            tn.write(b"end\r\n")
+            result = tn.read_until(b"end")
+            tn.close()
+            if "Invaild Slot." in str(result):
+                return "The Card number maybe unavailable or does not exist."
+            if "Can not get the fdb information." in str(result):
+                return "This Card is not connected."
+            result = str(result).split("\\r\\n")
+            result = [re.sub(r'\s+--P[a-zA-Z +\\1-9[;-]+H', '', val) for val in result if
+                      re.search(r'\s{4,}[-\d\w]|-{5,}|(All|Total)\W', val)]
+            for i in result:
+                if self.__mac in i:
+                    port_number = i.split()[1].split(":")[1].strip()
+                    return f"Port number of MAC '{self.__mac}' is: {port_number}"
+            return f"MAC address '{self.__mac}' does not exits"
+
+        except (EOFError, socket_error) as e:
+            print(e)
+            self.retry += 1
+            if self.retry < 4:
+                return self.run_command()
+
+        except Exception as e:
+            print(e)
+            return "error"
+
