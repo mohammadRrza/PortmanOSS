f9b91b520de06fbf0ab6761e15e6e89db190dcfb
MohammadReza <mrtbadboy>
2021-11-07 09:56:50 +0330

    radio

--- portman_web/contact/serializers.py
+++ portman_web/contact/serializers.py
@@ -20,5 +20,5 @@ class OrderSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Order
-        fields = ['rastin_order_id', 'order_contact_id', 'username', 'ranjePhoneNumber', 'slot_number', 'port_number',
-                  'telco_row', 'telco_column', 'telco_connection', 'fqdn']
+        fields = ['username', 'ranjePhoneNumber', 'slot_number', 'port_number',
+                  'telco_row', 'telco_column', 'telco_connection', 'fqdn', 'status']
