3c5514d30a09f63d83113e78e6a1d9b86a0d6efa
MohammadReza <mrtbadboy>
2021-11-09 13:57:45 +0330

    radio

--- portman_web/contact/admin.py
+++ portman_web/contact/admin.py
@@ -1,7 +1,10 @@
 from django.contrib import admin
-from contact.models import ContactType, Contact, Order, PortmapState
+from contact.models import ContactType, Contact, Order, PortmapState, Province, City, CenterType
 
 admin.site.register(ContactType)
 admin.site.register(Contact)
 admin.site.register(Order)
 admin.site.register(PortmapState)
+admin.site.register(Province)
+admin.site.register(City)
+admin.site.register(CenterType)
