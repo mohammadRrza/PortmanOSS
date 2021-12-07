06f9f5abd41cc19d35830cc306e764057b3a8ae5
MohammadReza <mrtbadboy>
2021-12-07 13:50:18 +0330

    log_model

--- portman_core2/vendors/fiberhomeAN5006_commands/show_port.py
+++ portman_core2/vendors/fiberhomeAN5006_commands/show_port.py
@@ -1,3 +1,5 @@
+import os
+import sys
 import telnetlib
 import time
 from socket import error as socket_error
@@ -133,11 +135,18 @@ class ShowPort(BaseCommand):
             return res
 
         except (EOFError, socket_error) as e:
+            exc_type, exc_obj, exc_tb = sys.exc_info()
+            fname = os.path.split(exc_tb.tb_frame.f_code.co_filename)[1]
+            print((str(exc_tb.tb_lineno)+'//1'))
             print(e)
             self.retry += 1
             if self.retry < 4:
                 return self.run_command()
 
         except Exception as e:
+            exc_type, exc_obj, exc_tb = sys.exc_info()
+            fname = os.path.split(exc_tb.tb_frame.f_code.co_filename)[1]
+            print((str(exc_tb.tb_lineno)+'//2'))
+            print(e)
             print(e)
             return str(e)
