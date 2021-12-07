a55faefe12879c960c7b64dbb915d0572c4f37fd
MohammadReza <mrtbadboy>
2021-12-07 14:00:37 +0330

    log_model

--- portman_core2/vendors/fiberhomeAN5006_commands/show_mac.py
+++ portman_core2/vendors/fiberhomeAN5006_commands/show_mac.py
@@ -65,14 +65,14 @@ class ShowMac(BaseCommand):
                 if "total: 0." in str(result):
                     return f"No MAC address is assigned to port '{self.port_conditions['port_number']}'"
                 tn.close()
+                if self.device_ip == '127.0.0.1' or self.device_ip == '172.28.238.114':
+                    return result.decode('utf-8')
                 result = str(result).split("\\r\\n")
                 result = [val for val in result if re.search(r'\s{3,}|--{4,}|:|learning', val)]
                 return result
             tn.write(b"\r\n")
             tn.write(b"end\r\n")
             result = tn.read_until(b"end")
-            if self.device_ip == '127.0.0.1' or self.device_ip == '172.28.238.114':
-                return result.decode('utf-8')
             if "invalid interface" in str(result):
                 str_res = ["There is one of the following problems:",
                            "This card is not configured or not available", "Card number is out of range."]
