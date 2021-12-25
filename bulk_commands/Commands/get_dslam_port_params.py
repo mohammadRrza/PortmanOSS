720fb4283e0b39585f6c39faf7c6dc0b56e07eb5
MohammadReza <mrtbadboy>
2021-12-25 15:24:19 +0330

    log_search

--- bulk_commands/Commands/get_dslam_port_params.py
+++ bulk_commands/Commands/get_dslam_port_params.py
@@ -2,22 +2,56 @@ import datetime
 import json
 import os
 import sys
-import paramiko
 from django.db import connection
-from django.http import JsonResponse
-from pathlib import Path
+from portman_web.dslam import utility
+
+from bulk_commands.dj_bridge import DSLAM
 
 
-class GetDslamPortParams():
-    def __init__(self):
-        pass
+class port_condition2():
+    slot_number = 0
+    port_number = 0
 
+
+class param2():
+    command = ''
+    type = ''
+    is_queue = ''
+    fqdn = ''
+    slot_number = 0
+    port_number = 0
+    port_conditions = port_condition2()
+
+
+class GetDslamPortParam():
     def run_command(self):
-        query = 'SELECT * from "16M_report"'
+        print('ssssssssssssssss')
+        query = 'SELECT * from "16M_report" where fqdn is not null'
         cursor = connection.cursor()
         cursor.execute(query)
         rows = cursor.fetchall()
         for row in rows:
-            print(row)
+            print(row[17])
+            dslam_obj = DSLAM.objects.get(fqdn=row[17].lower())
+            params = param2()
+            params.type = 'dslamport'
+            params.is_queue = False
+            params.fqdn = dslam_obj.fqdn
+            params.command = 'show linerate'
+            params.port_conditions = port_condition2()
+            params.port_conditions.slot_number = row[15]
+            params.port_conditions.port_number = row[16]
+            params = json.dumps(params, default=lambda x: x._dict_)
+            try:
+                result = utility.dslam_port_run_command(dslam_obj.pk, 'show linerate', json.loads(params))
+                print('+++++++++++++++++++++++++++')
+                print(result['result']['payloadrateDown'])
+                print('+++++++++++++++++++++++++++')
+                table_name = '"16m_result"'
+                query = "INSERT INTO public.{} VALUES ('{}', '{}', '{}', '{}')".format(table_name, row[17].lower(), row[16], row[15], result['result']['payloadrateDown'])
+                print(query)
+                cursor = connection.cursor()
+                cursor.execute(query)
 
-
+            except Exception as e:
+                print(e)
--- bulk_commands/bulk_commands.py
+++ bulk_commands/bulk_commands.py
@@ -5,7 +5,6 @@ from Commands.get_cisco_switches_backup import GetCiscoSwitchbackUp
 from Commands.get_zabbix_hosts import ZabbixHosts
 from Commands.get_mikrotik_radio_backup import GetMikrotikRadiobackUp
 from Commands.ip_service_set import SetIPService
-from bulk_commands.Commands.get_dslam_port_params import GetDslamPortParams
 
 if __name__ == '__main__':
     '''back_up = GetbackUp()
@@ -13,15 +12,15 @@ if __name__ == '__main__':
     back_up.run_command()
     ip_service_set = SetIPService()
     ip_service_set.run_command()'''
-    # zabbix_hosts = ZabbixHosts()
-    # zabbix_hosts.get_zabbix_hosts()
-    # cisco_switches_backup = GetCiscoSwitchbackUp()
-    # GetCiscoSwitchbackUp.run_command(self=None)
-    # mikrotik_routers_backup = GetMikrotikbackUp()
-    # mikrotik_routers_backup.run_command()
-    # get_vlan_brief = GetVlanBrief()
-    # get_vlan_brief.run_command()
-    # #radio_backup = GetMikrotikRadiobackUp()
-    # #radio_backup.run_command()
-    s = GetDslamPortParams()
-    s.run_command()
+
+    zabbix_hosts = ZabbixHosts()
+    zabbix_hosts.get_zabbix_hosts()
+    cisco_switches_backup = GetCiscoSwitchbackUp()
+    GetCiscoSwitchbackUp.run_command(self=None)
+    mikrotik_routers_backup = GetMikrotikbackUp()
+    mikrotik_routers_backup.run_command()
+    get_vlan_brief = GetVlanBrief()
+    get_vlan_brief.run_command()
+    #radio_backup = GetMikrotikRadiobackUp()
+    #radio_backup.run_command()
+
