deec489caf320f5583e9ca350ca6cc8937d51de9
MohammadReza <mrtbadboy>
2022-01-05 15:53:13 +0330

    SIP

--- portman_core2/vendors/huawei_commands/ngn_register_port.py
+++ portman_core2/vendors/huawei_commands/ngn_register_port.py
@@ -58,9 +58,9 @@ class NGNRegisterPort(BaseCommand):
         try:
             tn = telnetlib.Telnet(self.__HOST)
             if tn.read_until(b'>>User name:'):
-                tn.write((self.__telnet_username + "\r\n").encode('utf-8'))
+                tn.write((b'root' + "\r\n").encode('utf-8'))
             if tn.read_until(b'>>User password:'):
-                tn.write((self.__telnet_password + "\r\n").encode('utf-8'))
+                tn.write((b'mduadmin' + "\r\n").encode('utf-8'))
             tn.write(b"\r\n")
             tn.write(b"\r\n")
             tn.write(b"\r\n")
