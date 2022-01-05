a5dcf634591209df8eea6497055fb5755460186d
MohammadReza <mrtbadboy>
2022-01-05 16:07:06 +0330

    SIP

--- portman_core2/vendors/huawei_commands/sip_configuration.py
+++ portman_core2/vendors/huawei_commands/sip_configuration.py
@@ -65,6 +65,7 @@ class SIPConfiguration(BaseCommand):
             tn.write(b"\r\n")
             tn.write(b"\r\n")
             tn.write(b"eenable\r\n")
+            tn.write(b"enable\r\n")
             tn.write(b"config\r\n")
             tn.write(b"interface sip 0\r\n")
             tn.write(("If-sip attribute basic media-ip {} signal-ip {} signal-port 5000\r\n".format('192.161.1.2', '192.161.1.2')).encode('utf-8'))
