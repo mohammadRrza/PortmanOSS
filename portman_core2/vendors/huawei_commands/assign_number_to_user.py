dee1d9c8bd93145d82f5724e52e15b30842d7d82
MohammadReza <mrtbadboy>
2022-01-05 17:44:45 +0330

    SIP

--- portman_core2/vendors/huawei_commands/assign_number_to_user.py
+++ portman_core2/vendors/huawei_commands/assign_number_to_user.py
@@ -62,6 +62,7 @@ class AssignNumberToUser(BaseCommand):
                 tn.write((self.__telnet_username + "\r\n").encode('utf-8'))
             if tn.read_until(b'>>User password:'):
                 tn.write((self.__telnet_password + "\r\n").encode('utf-8'))
+            tn.write(b"eenable\r\n")
             tn.write(b"enable\r\n")
             tn.write(b"config\r\n")
             tn.write(b"esl user\r\n")
