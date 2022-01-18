a39da0b9710213eb7e57af49a6ea01b2b9be2395
MohammadReza <mrtbadboy>
2022-01-18 12:35:23 +0330

    SIP

--- portman_core2/vendors/huawei_commands/display_sippstnuser_reg_state.py
+++ portman_core2/vendors/huawei_commands/display_sippstnuser_reg_state.py
@@ -12,8 +12,6 @@ class DisplaySippstnuserRegState(BaseCommand):
         self.__telnet_password = None
         self.port_conditions = params.get('port_conditions')
         self.device_ip = params.get('device_ip')
-        self.__phone_number = self.port_conditions['ngn_phon_number']
-        self.__sip_password = self.port_conditions['ngn_password']
     @property
     def HOST(self):
         return self.__HOST
