824e365543fbb8c5bd5f74fc848ebe8e8015b07e
MohammadReza <mrtbadboy>
2022-02-22 10:51:07 +0330

    SIP

--- portman_core2/vendors/huawei_commands/show_linerate.py
+++ portman_core2/vendors/huawei_commands/show_linerate.py
@@ -69,13 +69,9 @@ class ShowLineRate(BaseCommand):
             output = str(result)
             while '#' not in str(result):
                 result = tn.read_until(b"#", 0.1)
-                output += str(result)
+                output += str(result.decode('utf-8'))
                 tn.write(b"y")
             result = output.split("\r\n")
-            if self.device_ip == '127.0.0.1' or self.device_ip == '172.28.238.114':
-                str_join = "\r\n"
-                str_join = str_join.join(result)
-                return dict(result=str_join, status=200)
             tn.write(b"quit\r\n")
             # result = '\n'.join(eval(repr(tn.read_until(b'Upstream total output power(dBm)')).replace(r"---- More ( Press 'Q' to break ) ----\x1b[37D                                     \x1b[37D","")).split("\r\n")[:-1])
             tn.write(b"quit\r\n")
@@ -142,7 +138,10 @@ class ShowLineRate(BaseCommand):
                     res['attenuationDown'] = val.split(":")[1].strip()
                 if "Upstream channel attenuation" in val:
                     res['attenuationUp'] = val.split(":")[1].strip()
-
+            if self.device_ip == '127.0.0.1' or self.device_ip == '172.28.238.114':
+                str_join = "\r\n"
+                str_join = str_join.join(res)
+                return dict(result=str_join, status=200)
             return dict(result=res, status=200)
         except (EOFError, socket_error) as e:
             print(e)
