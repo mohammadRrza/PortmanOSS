2a918cf91fd3aca352365e50ed36ba7489bc2bd2
MohammadReza <mrtbadboy>
2022-04-10 15:08:54 +0430

    SIP

--- portman_core2/vendors/fiberhomeAN5006_commands/set_fast_profiles.py
+++ portman_core2/vendors/fiberhomeAN5006_commands/set_fast_profiles.py
@@ -51,7 +51,7 @@ class SetFastProfiles(BaseCommand):
             if "Login Failed." in str(err1):
                 return dict(result="Telnet Username or Password is wrong! Please contact with core-access department.", status=500)
             tn.write(b"cd qos\r\n")
-            tn.write("attach adsl profile profile name {0} interface {1}/{2}\r\n\r\n".format(self.__lineprofile, self.port_conditions['slot_number'], self.port_conditions['port_number']).encode('utf-8'))
+            tn.write("attach adsl profile name {0} interface {1}/{2}\r\n\r\n".format(self.__lineprofile, self.port_conditions['slot_number'], self.port_conditions['port_number']).encode('utf-8'))
             tn.write("end\r\n".encode('utf-8'))
             result = tn.read_until(b"end")
             return dict(result=result, status=200)
