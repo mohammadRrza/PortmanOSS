b2e5734682f4e89449daa85f98af024dd0f8544e
MohammadReza <mrtbadboy>
2022-05-15 14:11:20 +0430

    SIP

--- portman_core2/vendors/fiberhomeAN5006_commands/show_mac.py
+++ portman_core2/vendors/fiberhomeAN5006_commands/show_mac.py
@@ -78,10 +78,10 @@ class ShowMac(BaseCommand):
             result = [re.sub(r"\s+--P[a-zA-Z +\\1-9[;-]+J|\s+--P[a-zA-Z +'b'\\1-9[;-]+J", '', val) for val in result if
                       re.search(r'\s{3,}|--{4,}|:|learning', val)]
             tn.close()
-            if self.device_ip == '127.0.0.1' or self.device_ip == '172.28.238.114':
-                str_join = "\\r\\n"
-                str_join = str_join.join(result)
-                return dict(result=str_join, status=200)
+            # if self.device_ip == '127.0.0.1' or self.device_ip == '172.28.238.114':
+            str_join = "\\r\\n"
+            str_join = str_join.join(result)
+            return dict(result=str_join, status=200)
             return dict(result=result, status=200)
 
         except (EOFError, socket_error) as e:
