12e91525cbeb86e82531789f3b6c8b53a2b24f47
4900638838 <@4900638838>
2022-05-15 16:48:19 +0430

    update show mac

--- portman_core2/vendors/fiberhomeAN5006_commands/show_shelf.py
+++ portman_core2/vendors/fiberhomeAN5006_commands/show_shelf.py
@@ -54,11 +54,13 @@ class ShowShelf(BaseCommand):
             tn.write(b"end\r\n")
             result = tn.read_until(b"end")
             tn.close()
-            if self.device_ip == '127.0.0.1' or self.device_ip == '172.28.238.114':
-                return dict(result=result.decode('utf-8'), status=200)
             result = str(result).split("\\r\\n")
 
             result = [val for val in result if re.search(r'\s+\d|^\w+\s{4,}|--+', val)]
+            # if self.device_ip == '127.0.0.1' or self.device_ip == '172.28.238.114':
+            #     str_join = "\r\n"
+            #     str_join = str_join.join(result)
+            #     return dict(result=str_join, status=200)
             return dict(result=result, status=200)
 
         except (EOFError, socket_error) as e:
