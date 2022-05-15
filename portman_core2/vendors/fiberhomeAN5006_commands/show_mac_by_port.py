c06761610a0f3fb4e82ec62815c7d2773f53d2d3
MohammadReza <mrtbadboy>
2022-05-15 17:22:16 +0430

    SIP

--- portman_core2/vendors/fiberhomeAN5006_commands/show_mac_by_port.py
+++ portman_core2/vendors/fiberhomeAN5006_commands/show_mac_by_port.py
@@ -85,10 +85,10 @@ class ShowMacSlotPort(BaseCommand):
                 return dict(result=f"No MAC address is assigned to port '{self.port_conditions['port_number']}'", status=500)
             tn.close()
             result = str(result).split("\\r\\n")
-            # if self.device_ip == '127.0.0.1' or self.device_ip == '172.28.238.114':
-            #     str_join = "\\r\\n"
-            #     str_join = str_join.join(result)
-            #     return dict(result=str_join, status=200)
+            if self.device_ip == '127.0.0.1' or self.device_ip == '172.28.238.114':
+                str_join = "\r\n"
+                str_join = str_join.join(result)
+                return dict(result=str_join, status=200)
             return dict(result=result, status=200)
 
         except (EOFError, socket_error) as e:
