90882e2b08f0afbac2595b13ca23a3d226161245
MohammadReza <mrtbadboy>
2022-06-20 12:29:29 +0430

    All_dlsms

--- portman_async/portman_async.py
+++
@@ -1,5 +0,0 @@
-from celery import Celery
-
-if __name__ == "__main__":
-    print(Celery.tasks)
-    print('Helloddd')
--- portman_async/tasks/dslam_tasks/dslam_tasks.py
+++
@@ -1,7 +0,0 @@
-from celery import shared_task
-import pika
-
-
-@shared_task()
-def get_zyxel_ports_info():
-    pass
