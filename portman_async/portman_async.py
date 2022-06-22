5b924f02ec9de70fd3369422415dd64180f6ab38
MohammadReza <mrtbadboy>
2022-06-22 12:55:37 +0430

    SNMP

--- portman_async/portman_async.py
+++ portman_async/portman_async.py
@@ -43,12 +43,12 @@ def get_current_port_status():
                 try:
                     result = utility.dslam_port_run_command(dslam_obj.pk, 'snmp get port params', json.loads(params))
                     print(result)
-                    return result
+                    # return result
                 except Exception as ex:
                     exc_type, exc_obj, exc_tb = sys.exc_info()
                     fname = os.path.split(exc_tb.tb_frame.f_code.co_filename)[1]
                     print(ex, exc_tb.tb_lineno)
-                    return 'ERROR'
+                    # return 'ERROR'
             print('OKKKKKKKK')
 
 
